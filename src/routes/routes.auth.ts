f4c060c17d845758c49d7fbaf14cbc6bc388e0c4
DeMo900 <dodoadam893@gmail.com>
2026-09-23 18:37:34 +0300

    adding signup endpoint

--- src/routes/routes.auth.ts
+++ src/routes/routes.auth.ts
@@ -1,4 +1,4 @@
-import { Hono } from "hono";
+import { type Context, Hono } from "hono";
 import { sign } from "hono/jwt";
 import * as AuthServices from "../services/services.auth";
 import { sendError, sendSuccess } from "../utils/response";
@@ -57,3 +57,61 @@ AuthRoutes.post("/auth/login", async (c) => {
     "Login successful.",
   );
 });
+
+const handleSignup = async (c: Context<{ Bindings: Env }>) => {
+  let body: any;
+  try {
+    body = await c.req.json();
+  } catch {
+    return sendError(c, "Invalid JSON body provided.", 400, "BAD_REQUEST");
+  }
+
+  if (!body || !body.username || !body.password) {
+    return sendError(
+      c,
+      "Username and password are required.",
+      400,
+      "BAD_REQUEST",
+    );
+  }
+
+  const user = await AuthServices.signUp(
+    body.username,
+    body.password,
+    c.env.ravin_db,
+  );
+
+  if (!user) {
+    return sendError(
+      c,
+      "Username already exists or failed to create user.",
+      400,
+      "USER_CREATION_FAILED",
+    );
+  }
+
+  const expirationDate = 60 * 60 * 24 * 30;
+  const token = await sign(
+    {
+      id: user.id,
+      username: user.username,
+      exp: Math.floor(Date.now() / 1000 + expirationDate),
+    },
+    c.env.JWT_SECRET,
+  );
+
+  return sendSuccess(
+    c,
+    {
+      token,
+      user,
+    },
+    201,
+    "Admin user created successfully.",
+  );
+};
+
+AuthRoutes.post("/auth/signup", handleSignup);
+AuthRoutes.post("/signup", handleSignup);
+
+
